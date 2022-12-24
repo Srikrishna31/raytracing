bb551792e5e402d09f4204de2c454e5d38da593c
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-24 07:45:29 +0100

    Added progress indicator for writing image

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("P3\n{} {}\n255\n", &IMAGE_WIDTH, &IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("\rScanlines remaining: {} ", &j);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -18,6 +19,7 @@ fn main() {
 
             println!("{ir} {ig} {ib}\n");
         }
+        eprintln!("\nDone.\n")
     }
 
 }
