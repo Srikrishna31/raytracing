757d2fa661d831c44e935f1500c0967d4188d2dd
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-24 07:38:49 +0100

    Fix the down counter loop for rendering image

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     // Render
     println!("P3\n{} {}\n255\n", &IMAGE_WIDTH, &IMAGE_HEIGHT);
 
-    for i in 0..IMAGE_HEIGHT {
-        for j in 0..IMAGE_WIDTH {
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let b = 0.25;
