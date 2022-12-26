50136fea8f8204b447405c9afdf9fffeaba3f9f2
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-26 04:19:17 +0100

    Use Color structure to write color to standard output

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod vec3;
 use std::fmt::Write as FmtWrite;
+use std::io;
 use std::io::{Result, Write};
+use vec3::Color;
 
 pub fn write_color<T: Write>(out: &mut T, pixel_color: &vec3::Color) -> Result<usize> {
     let mut str = String::new();
@@ -14,4 +16,27 @@ pub fn write_color<T: Write>(out: &mut T, pixel_color: &vec3::Color) -> Result<u
     .expect("Error formatting write");
 
     out.write(str.as_bytes())
+}
+
+pub fn write_image() {
+    // Image
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    // Render
+    println!("P3\n{} {}\n255\n", &IMAGE_WIDTH, &IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("\rScanlines remaining: {} ", &j);
+        for i in 0..IMAGE_WIDTH {
+            let pixel_color = Color::new(
+                i as f64 / (IMAGE_WIDTH - 1) as f64,
+                j as f64 / (IMAGE_HEIGHT - 1) as f64,
+                0.25,
+            );
+
+            write_color(&mut io::stdout(), &pixel_color).expect("Error writing to output");
+        }
+        eprintln!("\nDone.\n")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
+use raytracing::write_image;
+
 fn main() {
-    // Image
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
-
-    // Render
-    println!("P3\n{} {}\n255\n", &IMAGE_WIDTH, &IMAGE_HEIGHT);
-
-    for j in (0..IMAGE_HEIGHT).rev() {
-        eprintln!("\rScanlines remaining: {} ", &j);
-        for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{ir} {ig} {ib}\n");
-        }
-        eprintln!("\nDone.\n")
-    }
+    write_image()
 }
