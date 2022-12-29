bd30e293b5e7faf402e956100ab57427e2585899
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-29 04:50:48 +0100

    Add ray sphere intersection

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,59 @@ pub fn write_image() {
 /// of those rays. The involved steps are (1) calculate the ray from the eye to the pixel, (2) determine
 /// which objects the ray intersects, and (3) compute a color for that intersection point.
 pub fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+/// # Ray-Sphere Intersection
+/// The equation for a sphere centered at the origin of radius **R** is *x<sup>2</sup> + y<sup>2</sup> + z<sup>2</sup> = R<sup>2</sup>*
+/// Put another way, if a given point (x,y,z) is on the sphere, then *x<sup>2</sup> + y<sup>2</sup> + z<sup>2</sup> = R<sup>2</sup>*.
+/// If the given point is *inside* the sphere, then *x<sup>2</sup> + y<sup>2</sup> + z<sup>2</sup> < R<sup>2</sup>*,
+/// and if a given point is *outside* the sphere, then *x<sup>2</sup> + y<sup>2</sup> + z<sup>2</sup> >R<sup>2</sup>*. It gets
+/// uglier if the sphere center is at (C<sub>x</sub>, C<sub>y</sub>, C<sub>z</sub>):
+///
+/// *(x - C<sub>x</sub>)<sup>2</sup> + (y - C<sub>y</sub>)<sup>2</sup> + (z - C<sub>z</sub>)<sup>2</sup> = r<sup>2</sup>*
+///
+/// In graphics, you almost always want your formulas to be in terms of vectors so all the *x/y/z* stuff
+/// is under the hood in the `Vec3` class. The vector from center **C**=(C<sub>x</sub>,C<sub>y</sub>,C<sub>z</sub>)
+/// to point **P** = (*x,y,z*) is (**P** - **C**), and therefore
+///
+/// (**P** - **C**) . (**P** - **C**) = (*x - C<sub>x</sub>*)<sup>2</sup> + (*y - C<sub>y</sub>*)<sup>2</sup> + (*z - C<sub>z</sub>*)<sup>2</sup>.
+///
+/// So,the equation of the sphere in the vector form is:
+///
+/// (**P - C**).(**P - C**) = r<sup>2</sup>
+///
+/// We can read this as "any point **P** that satisfies this equation is on the sphere". We want to
+/// know if our ray **P**(*t*) = **A** + *t***b** ever hits the sphere anywhere. If it does hit the
+/// sphere, there is some *t* for which **P**(*t*) satisfies the sphere equation. So we are looking
+/// for any *t* where this is true:
+///
+/// (**P**(*t*) - **C**).(**P**(*t*) - **C**) = r<sup>2</sup>
+///
+/// or expanding the full form of the ray **P**(*t*):
+///
+/// (**A** + *t***b** - **C**).(**A** + *t***b** - **C**) = *r*<sup>2</sup>
+///
+/// The rules of vector algebra are all that we want here. If we expand that equation and move all
+/// the terms to the left hand side we get:
+///
+/// *t<sup>2</sup>***b**.**b** + 2*t***b**.(**A - C**) + (**A - C**).(**A - C**) - r<sup>2</sup> = 0
+///
+/// The vectors and *r* in that equation are all constant and known. The unknown is t, and the equation
+/// is a quadratic. You can solve for *t* and there is a square root part that is either positive
+/// (meaning two real solutions), negative (meaning no real solutions), or zero (meaning one real
+/// solution).
+pub fn hit_sphere(center: Point, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin() - center;
+    let a = ray.direction().dot(&ray.direction());
+    let b = 2.0 * oc.dot(&ray.direction());
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b*b - 4.0*a*c;
+
+    discriminant > 0.0
 }
