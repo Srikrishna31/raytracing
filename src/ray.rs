a843e15f9f593235be865fbd9cbbaf3e03e8e57b
Krishna Addepalli <coolkrishna31@gmail.com>
2023-02-02 07:05:32 +0100

    Add image rendering capability to doc strings

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,5 @@
 use crate::vec3::{Point, Vec3};
+use embed_doc_image::embed_doc_image;
 
 /// The one thing that all ray tracers have is a ray class and a computation of what color is seen
 /// along a ray. We can think of a ray as a function: **P**(*t*) = **A** + *t***b**.
@@ -7,7 +8,9 @@ use crate::vec3::{Point, Vec3};
 /// direction. The ray parameter *t* is a real number (`f64` in code). Plug in a different *t* and
 /// **P**(*t*) moves the point along the ray. Add in negative *t* values and you can go anywhere on
 /// the 3D line. For positive *t*, you get only the parts in front of **A**, and this is what is
-/// often called a half-line or ray.a
+/// often called a half-line or ray.
+/// ![Linear Interpolation][lerp]
+#[embed_doc_image("lerp", "doc_images/linear_interpolation.jpg")]
 pub struct Ray {
     origin: Point,
     direction: Vec3,
