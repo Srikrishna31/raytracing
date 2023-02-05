ae63051160954db130bc01106f4fb7d82e36f0d7
Krishna Addepalli <coolkrishna31@gmail.com>
2023-02-05 18:04:05 +0100

    Add missed out documentation for Dielectrics

--- src/materials/dielectric.rs
+++ src/materials/dielectric.rs
@@ -21,6 +21,71 @@ use embed_doc_image::embed_doc_image;
 /// ```math
 ///     sinθ' = \frac{η}{η'} . sinθ
 /// ```
+///
+/// On the refracted side of the surface there is a refracted ray **R'** and a normal **n'**, and
+/// there exists an angle, **θ'**, between them. We can split **R'** into the parts of the ray that
+/// are perpendicular to **n'** and parallel to **n'**:
+///
+/// ```math
+///     R' = R⟂ + R∥
+/// ```
+///
+/// If we solve for $R'\perp$ and $R'\parallel$ we get:
+///
+/// ```math
+///     R'⟂ = \frac{η}{η'}(R + cosθn)
+///
+///     R'∥ = -√(1- |R'⟂|²)
+///
+/// We still need to solve for **cosθ**. It is well known that the dot product of two vectors can be
+/// explained in terms of the cosine of the angle between them:
+///
+/// ```math
+///     a.b = |a||b|cosθ
+/// ```
+///
+/// We can now rewrite **R'**⟂ in terms of known quantities:
+///
+/// ```math
+///     R'⟂ = \frac{η}{η'}(R + (-R.n)n)
+/// ```
+///
+/// When we combine them back together, we can write a function to calculate **R'**
+///
+/// # Total Internal Reflection
+///
+/// One troublesome practical issue is that when the ray is in the material with the higher refractive
+/// index, there is no real solution to Snell's law, and thus there is no refraction possible. If we
+/// refer back to Snell's law and the derivation of sinθ':
+///
+/// ```math
+///     sinθ' = \frac{η}{η'}.sinθ
+/// ```
+///
+/// If the ray is inside glass and outside is air (η=1.5 and η'=1.0):
+///
+/// ```math
+///     sinθ' = \frac{1.5}{1.0}.sinθ
+/// ```
+///
+/// The value of **sinθ'** cannot be greater than 1. So, if,
+///
+/// ```math
+///     \frac{1.5}{1.0}.sinθ > 1.0
+/// ```
+/// , the equality between the two sides of the equation is broken, and a solution cannot exist. If α
+/// solution doesnot exist, **the glass cannot refract, and therefore must reflect the ray**.
+///
+/// Here all the light is reflected, and because in practice that is usually inside solid objects, it
+/// is called "total internal reflection". This is why sometimes the water-air boundary acts as a
+/// perfect mirror when you are submerged.
+///
+/// We can solve for `sin_theta` using the trigonometric qualities:
+///
+/// ```math
+///     sinθ = √(1 - cos²θ)
+///     cosθ = R.n
+/// ```
 #[embed_doc_image("rayrefract", "doc_images/ray_refraction.jpg")]
 #[derive(Debug, Clone, Copy)]
 pub struct Dielectric {
