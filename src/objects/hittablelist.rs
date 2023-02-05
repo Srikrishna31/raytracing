49dc5c80e436f57dea3ca651f1b1945e789e6f6c
Krishna Addepalli <coolkrishna31@gmail.com>
2023-02-05 17:31:06 +0100

    Refactor HittableList::hit function to use fold

--- src/objects/hittablelist.rs
+++ src/objects/hittablelist.rs
@@ -11,32 +11,17 @@ pub struct HittableList {
 
 impl Hittable for HittableList {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
-        // self.objects.iter().fold(None, |acc, val| {
-        //     let h = val.hit(r, t);
-        //     if h.is_some() {
-        //         h
-        //     } else {
-        //         acc
-        //     }
-        // })
-
-        let mut temp_rec: HitRecord = HitRecord::default();
-        let mut hit_anything = false;
         let mut closest_so_far = t_max;
 
-        for obj in self.objects.iter() {
-            if let Some(rec) = obj.hit(r, t_min, closest_so_far) {
-                hit_anything = true;
+        // TODO: Investigate a way to parallelize this function.
+        self.objects.iter().fold(None, |acc, val| {
+            if let Some(rec) = val.hit(r, t_min, closest_so_far) {
                 closest_so_far = rec.t;
-                temp_rec = rec;
+                Some(rec)
+            } else {
+                acc
             }
-        }
-
-        if hit_anything {
-            Some(temp_rec)
-        } else {
-            None
-        }
+        })
     }
 }
 
