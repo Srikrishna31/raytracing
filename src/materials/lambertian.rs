469d2172eb51e9454507c6b01ba6046e8e365617
Krishna Addepalli <coolkrishna31@gmail.com>
2023-02-11 10:30:35 +0100

    Change the color attribute of Lambertian to SolidColor Texture

--- src/materials/lambertian.rs
+++ src/materials/lambertian.rs
@@ -1,14 +1,16 @@
 use crate::materials::material;
 use crate::objects::HitRecord;
+use crate::textures::{SolidColor, Texture};
 use crate::{Color, Ray, Vec3};
 use material::Material;
+use std::rc::Rc;
 
 /// The Lambertian material, it can either scatter always and attenuate by its reflectance **R**, or
 /// it can scatter with no attenuation but absorb the fraction **1 - R** of the rays, or it could be
 /// a mixture of those strategies.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct LambertianMaterial {
-    albedo: Color,
+    albedo: Rc<SolidColor>,
 }
 
 impl Material for LambertianMaterial {
@@ -22,13 +24,15 @@ impl Material for LambertianMaterial {
 
         Some((
             Ray::new_with_time(&rec.p, &scatter_direction, r_in.time()),
-            self.albedo,
+            self.albedo.value(rec.u, rec.v, &rec.p),
         ))
     }
 }
 
 impl LambertianMaterial {
     pub fn new(a: Color) -> LambertianMaterial {
-        LambertianMaterial { albedo: a }
+        LambertianMaterial {
+            albedo: Rc::new(SolidColor::new(a)),
+        }
     }
 }
