b3195aedc963594db6a9c18a559d791765c5dd8a
Krishna Addepalli <coolkrishna31@gmail.com>
2023-02-11 12:36:10 +0100

    Add Perlin noise support class

--- src/textures/mod.rs
+++ src/textures/mod.rs
@@ -2,8 +2,9 @@ mod checker_texture;
 mod solid_color;
 mod texture;
 mod perlin;
+mod perlin_noise_texture;
 
 pub use checker_texture::CheckerTexture;
 pub use solid_color::SolidColor;
 pub use texture::Texture;
-pub use perlin::Perlin;
+
