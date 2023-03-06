dac5d7e72b012e171a967886b72dc89fbb96e78a
Krishna Addepalli <coolkrishna31@gmail.com>
2023-03-06 19:46:35 +0100

    Added example for rtnextweek

--- raytracer/examples/rtnextweek.rs
+++ raytracer/examples/rtnextweek.rs
@@ -5,7 +5,7 @@ use scenes::Scenes;
 
 fn main() {
     let time = timeit_loops!(1, {
-        scenes::render_scene("rtweekendone.jpg", Scenes::RTWeekendOneFinalScene);
+        scenes::render_scene("rtnextweek.jpg", Scenes::RTNextWeekFinalScene);
     });
     eprintln!("{time} seconds to render the image");
 }
